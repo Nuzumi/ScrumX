7c0540b59d86b6701d0a8c6b35db12a1951ddc4c
Nuzumi <michal.sewerniak@o2.pl>
2017-10-30 12:45:12 +0100

    dodano przechodzenie miedzy oknami w LoginWindow dodano linijke z ponownym wpisaniem hasla

---
+++ ScrumX/ViewModel/BacklogVM.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrumX.API.Model;
+using System.Windows.Input;
+using Prism.Commands;
+using System.Windows;
+using ScrumX.ViewModel;
+using ScrumX.View;
+
+namespace ScrumX.ViewModel
+{
+    class BacklogVM : BindableBase
+    {
+        private User logedUser;
+
+        #region Properties
+
+        public string UserName { get; set; }
+
+        public ICommand GoToTableCommand { get; set; }
+        #endregion
+
+        public BacklogVM(User user)
+        {
+            GoToTableCommand = new DelegateCommand<Window>(GoToTableExecute);
+            logedUser = user;
+            UserName = user.Name;
+        }
+
+        #region CommandFunctions
+
+        private void GoToTableExecute(Window window)
+        {
+            TableVM dataContext = new TableVM(logedUser);
+            Table table = new Table();
+            table.DataContext = dataContext;
+            table.Show();
+            window.Close();
+        }
+
+        #endregion
+    }
+}
