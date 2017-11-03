38d93af6deb86f1c690e5ef1175397d08229f3ec
Nuzumi <michal.sewerniak@o2.pl>
2017-11-03 01:25:38 +0100

    added dzialajacy przycisk "add task" wyswietlanie okna AddTask.xaml

--- ScrumX/ViewModel/BacklogVM.cs
+++ ScrumX/ViewModel/BacklogVM.cs
@@ -21,19 +21,39 @@ namespace ScrumX.ViewModel
 
         public string UserName { get; set; }
 
+        private bool canAddTask;
+        public bool CanAddTask
+        {
+            get { return canAddTask; }
+            set
+            {
+                SetProperty(ref canAddTask, value);
+                (AddTaskCommand as DelegateCommand).RaiseCanExecuteChanged();
+                (GoToTableCommand as DelegateCommand<Window>).RaiseCanExecuteChanged();
+            }
+        }
+
         public ICommand GoToTableCommand { get; set; }
+        public ICommand AddTaskCommand { get; set; }
         #endregion
 
         public BacklogVM(User user)
         {
-            GoToTableCommand = new DelegateCommand<Window>(GoToTableExecute);
+            GoToTableCommand = new DelegateCommand<Window>(GoToTableCommandExecute,GoToTableCommandCanExecute);
+            AddTaskCommand = new DelegateCommand(AddTaskCommandExecute, AddTaskCommandCanExecute);
+            CanAddTask = true;
             logedUser = user;
             UserName = user.Name;
         }
 
+        public void changeCanAddTaskToTrue()
+        {
+            CanAddTask = true;
+        }
+
         #region CommandFunctions
 
-        private void GoToTableExecute(Window window)
+        private void GoToTableCommandExecute(Window window)
         {
             TableVM dataContext = new TableVM(logedUser);
             Table table = new Table();
@@ -42,6 +62,24 @@ namespace ScrumX.ViewModel
             window.Close();
         }
 
+        private bool GoToTableCommandCanExecute(Window dummy)
+        {
+            return CanAddTask;
+        }
+
+        private void AddTaskCommandExecute()
+        {
+            CanAddTask = false;
+            AddTaskVM dataContext = new AddTaskVM(changeCanAddTaskToTrue);
+            AddTask dialog = new AddTask();
+            dialog.DataContext = dataContext;
+            dialog.Show();
+        }
+
+        private bool AddTaskCommandCanExecute()
+        {
+            return CanAddTask;
+        }
         #endregion
     }
 }
--- ScrumX/ViewModel/TableVM.cs
+++ ScrumX/ViewModel/TableVM.cs
@@ -8,6 +8,7 @@ using ScrumX.API.Model;
 using System.Windows.Input;
 using System.Windows;
 using Prism.Commands;
+using ScrumX.View;
 
 namespace ScrumX.ViewModel
 {
@@ -19,27 +20,65 @@ namespace ScrumX.ViewModel
 
         public string UserName { get; set; }
 
+        private bool canAddTask = true;
+        public bool CanAddTask
+        {
+            get { return canAddTask; }
+            set
+            {
+                SetProperty(ref canAddTask, value);
+                (AddTaskCommand as DelegateCommand).RaiseCanExecuteChanged();
+                (GoToBacklogCommand as DelegateCommand<Window>).RaiseCanExecuteChanged();
+            }
+        }
+
         public ICommand GoToBacklogCommand { get; set; }
+        public ICommand AddTaskCommand { get; set; }
 
         #endregion
 
 
         public TableVM(User user)
         {
-            GoToBacklogCommand = new DelegateCommand<Window>(goToBacklogCommandExecute);
+            GoToBacklogCommand = new DelegateCommand<Window>(GoToBacklogCommandExecute,GoToBacklogCommandCanExecute);
+            AddTaskCommand = new DelegateCommand(AddTaskCommandExecute, AddTaskCommandCanExecute);
             logedUser = user;
             UserName = user.Name;
         }
 
+        public void changeCanAddTaskToTrue()
+        {
+            CanAddTask = true;
+        }
+
         #region CommandFunctions
 
-        private void goToBacklogCommandExecute(Window window)
+        private void GoToBacklogCommandExecute(Window window)
         {
             BacklogVM dataContext = new BacklogVM(logedUser); // mozna by przekazywac ta wczesniej uzywana klase by parametry wyszukiwania zostawaly
             Backlog backlog = new Backlog();
             backlog.DataContext = dataContext;
             backlog.Show();
             window.Close();
+        }
+
+        private bool GoToBacklogCommandCanExecute(Window dummy)
+        {
+            return CanAddTask;
+        }
+
+        private void AddTaskCommandExecute()
+        {
+            CanAddTask = false;
+            AddTaskVM dataContext = new AddTaskVM(changeCanAddTaskToTrue);
+            AddTask dialog = new AddTask();
+            dialog.DataContext = dataContext;
+            dialog.Show();
+        }
+
+        private bool AddTaskCommandCanExecute()
+        {
+            return CanAddTask;
         }
 
         #endregion
